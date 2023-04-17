db43a9df8402b74fcfa9b52899032209d8e70469
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-17 09:25:56 +0300

    dynamic routes

---
+++ pages/product/index.js
@@ -0,0 +1,16 @@
+// dynamic routes
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h1>PRODUCT 1 </h1>
+        <h1>PRODUCT 2 </h1>
+        <h1>PRODUCT 3 </h1>
+        <h1>PRODUCT 4</h1>
+
+    </div>
+  )
+}
+
+export default index
